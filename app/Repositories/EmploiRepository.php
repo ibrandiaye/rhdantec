2439b35f271b7175b5d54c852f9b008ca0454d4e
Ibra Ndiaye <ibra789diaye@gmail.com>
2024-10-01 17:42:34 +0000

    test 4

--- app/Repositories/EmploiRepository.php
+++ app/Repositories/EmploiRepository.php
@@ -3,10 +3,30 @@ namespace App\Repositories;
 
 use App\Models\Emploi;
 use App\Repositories\RessourceRepository;
+use Illuminate\Support\Facades\DB;
 
 class EmploiRepository extends RessourceRepository{
     public function __construct(Emploi $emploi){
         $this->model = $emploi;
     }
- 
+
+    public function getEmploiByIdentification($id){
+        return DB::table("emplois")
+        ->join("bureaus","emplois.bureau_id","=","bureaus.id")
+        ->join("csps","emplois.csp_id","=","csps.id")
+        ->join("divisions","emplois.division_id","=","divisions.id")
+        ->join("employeurs","emplois.employeur_id","=","employeurs.id")
+        ->join("famille_pros","emplois.famille_pro_id","=","famille_pros.id")
+        ->join("fonctions","emplois.fonction_id","=","fonctions.id")
+        ->join("identifications","emplois.identification_id","=","identifications.id")
+        ->join("nature_contrats","emplois.nature_contrat_id","=","nature_contrats.id")
+        ->join("responsabilites","emplois.responsabilite_id","=","responsabilites.id")
+        ->join("services","emplois.service_id","=","services.id")
+        ->join("titres","emplois.titre_id","=","titres.id")
+        ->join("type_contrats","emplois.type_contrat_id","=","type_contrats.id")
+        ->select("emplois.*","csps.nom as csp","bureaus.nom as bureau","divisions.nom as division","employeurs.nom as employeur","famille_pros.nom as famille_pro","fonctions.nom as fonction","identifications.nom as identification"
+        ,"nature_contrats.nom as natureContrat","responsabilites.nom as responsabilite","services.nom as nom","titres.nom as titre","type_contrats.nom as typeContrat")
+        ->where("emplois.id","=", $id)
+        ->get();
+    }
 }
--- resources/views/identification/show.blade.php
+++ resources/views/identification/show.blade.php
@@ -20,7 +20,38 @@
 </div>
 
 <div class="row">
-    <div class="col-md-6 col-lg-6 col-xl-3">
+    <div class="col-md-4 col-lg-4 col-xl-4">
+
+        <div class="card">
+         {{--    <img class="card-img-top img-fluid" src="assets/images/small/img-2.jpg" alt="Card image cap"> --}}
+            <div class="card-body">
+                <h4 class="card-title font-20 mt-0">Identification</h4>
+              {{--   <p class="card-text">Some quick example text to build on the card title and make
+                    up the bulk of the card's content.</p>
+                </div> --}}
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Nom : <strong>{{ $identification->prenom }} {{ $identification->nom }}</strong></li>
+                    <li class="list-group-item">Matricule : <strong>{{ $identification->matricule }}</strong></li>
+                    <li class="list-group-item">CNI : <strong>{{ $identification->cni }}</strong></li>
+                    <li class="list-group-item">Matricule Universite : <strong>{{ $identification->matricule_universite }}</strong></li>
+                    <li class="list-group-item">Date Naissance : <strong>{{ $identification->datenaiss }} à {{ $identification->lieunaiss }}</strong></li>
+                    <li class="list-group-item">Genre : <strong>{{ $identification->sexe }}</strong></li>
+                    <li class="list-group-item">SM : <strong>{{ $identification->sm }}</strong></li>
+                    <li class="list-group-item">Adresse : <strong>{{ $identification->adresse }}</strong></li>
+                    <li class="list-group-item">telephone : <strong>{{ $identification->telephone }}</strong></li>
+                    <li class="list-group-item">Email : <strong>{{ $identification->email }}</strong></li>
+                    <li class="list-group-item">Religion : <strong>{{ $identification->religion }}</strong></li>
+
+                </ul>
+            {{--  <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div> --}}
+            </div>
+        </div>
+
+    </div><!-- end col -->
+    <div class="col-md-4 col-lg-4 col-xl-4">
 
         <div class="card">
          {{--    <img class="card-img-top img-fluid" src="assets/images/small/img-2.jpg" alt="Card image cap"> --}}
@@ -47,6 +78,38 @@
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
             </div> --}}
+        </div>
+        </div>
+
+    </div><!-- end col -->
+    <div class="col-md-4 col-lg-4 col-xl-4">
+
+        <div class="card">
+         {{--    <img class="card-img-top img-fluid" src="assets/images/small/img-2.jpg" alt="Card image cap"> --}}
+            <div class="card-body">
+                <h4 class="card-title font-20 mt-0">Identification</h4>
+              {{--   <p class="card-text">Some quick example text to build on the card title and make
+                    up the bulk of the card's content.</p>
+            </div> --}}
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nom : <strong>{{ $identification->prenom }} {{ $identification->nom }}</strong></li>
+                <li class="list-group-item">Matricule : <strong>{{ $identification->matricule }}</strong></li>
+                <li class="list-group-item">CNI : <strong>{{ $identification->cni }}</strong></li>
+                <li class="list-group-item">Matricule Universite : <strong>{{ $identification->matricule_universite }}</strong></li>
+                <li class="list-group-item">Date Naissance : <strong>{{ $identification->datenaiss }} à {{ $identification->lieunaiss }}</strong></li>
+                <li class="list-group-item">Genre : <strong>{{ $identification->sexe }}</strong></li>
+                <li class="list-group-item">SM : <strong>{{ $identification->sm }}</strong></li>
+                <li class="list-group-item">Adresse : <strong>{{ $identification->adresse }}</strong></li>
+                <li class="list-group-item">telephone : <strong>{{ $identification->telephone }}</strong></li>
+                <li class="list-group-item">Email : <strong>{{ $identification->email }}</strong></li>
+                <li class="list-group-item">Religion : <strong>{{ $identification->religion }}</strong></li>
+
+            </ul>
+           {{--  <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div> --}}
+        </div>
         </div>
 
     </div><!-- end col -->
